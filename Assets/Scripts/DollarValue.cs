df52a2aef26ffef2e5340d2337e93562fd16b3c0
kennedyat <ktpenny8@gmail.com>
2024-02-09 00:44:34 -0500

    Assigns Value to Spawned Dollar

---
+++ Assets/Scripts/DollarValue.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollarValue : MonoBehaviour
+{
+    public int value;
+    // Start is called before the first frame update
+    public void AddValue(int assigned_Value){
+        value = assigned_Value;
+    }
+}
