54e575d1bb9f50099a817c641c379dc29e53bd04
kennedyat <ktpenny8@gmail.com>
2024-02-16 15:26:24 -0500

    Update DollarValue.cs

--- Assets/Scripts/DollarValue.cs
+++ Assets/Scripts/DollarValue.cs
@@ -5,8 +5,34 @@ using UnityEngine;
 public class DollarValue : MonoBehaviour
 {
     public int value;
+    Renderer _mat;
+
+    void Start(){
+        _mat = GetComponent<Renderer>();
+
+    }
     // Start is called before the first frame update
     public void AddValue(int assigned_Value){
         value = assigned_Value;
+         _mat = GetComponent<Renderer>();
+        GetMaterial();
+    }
+
+    public void GetMaterial(){
+        switch(value){
+            case 20:
+                _mat.material.color = new Color(0,0,255);
+                break;
+            case 10:
+                _mat.material.color = new Color(0,255,0);
+                break;
+            case 5:
+                _mat.material.color = new Color(255,0,0);
+                break;
+            case 1:
+                _mat.material.color = new Color(255,138,248);
+                break;
+            
+            }
     }
 }
