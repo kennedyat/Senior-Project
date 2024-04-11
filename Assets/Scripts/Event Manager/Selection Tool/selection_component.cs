d72b5344febc7aaa99f1953f5c099ecde10941e7
kennedyat <ktpenny8@gmail.com>
2024-04-11 15:36:44 -0400

    Money Dollar Selection Material Change

--- Assets/Scripts/Event Manager/Selection Tool/selection_component.cs
+++ Assets/Scripts/Event Manager/Selection Tool/selection_component.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,31 @@ public class selection_component : MonoBehaviour
 {
     [SerializeField]
     private Material outliner;
+    private GameObject face = null;
+    private GameObject child = null;
+    Material originalMat;
+    Material currentMat;
 
     void Start()
     {
-        GetComponent<Renderer>().material = outliner;
+         //Get Child & Face of Child of Grabbed Object -- Only for Dollars!!!
+        if( this.transform.childCount>0){
+            child = this.transform.GetChild(0).gameObject;
+            face = getFaceObject(child);
+        }else{
+            Debug.Log("Coins");
+            face = transform.gameObject;
+        }
+       
+
+
+        if (face != null){
+            Debug.Log("Face");
+            originalMat = new Material(face.GetComponent<Renderer>().materials[0]) ;
+            currentMat = face.GetComponent<Renderer>().materials[0];
+            setFaceRender();
+        }
+        
         transform.parent = GameObject.FindGameObjectWithTag("Parent").transform;
         transform.parent.gameObject.GetComponent<GroupValue>().AddValue(gameObject.GetComponent<DollarValue>().value);
     }
@@ -17,8 +39,24 @@ public class selection_component : MonoBehaviour
     //This returns the texture when deselected
     private void OnDestroy()
     {
-        GetComponent<Renderer>().material = null;
+        face.GetComponent<Renderer>().materials[0] = originalMat;
         transform.parent.gameObject.GetComponent<GroupValue>().SubtractValue(gameObject.GetComponent<DollarValue>().value);
         transform.parent = null;
+    }
+
+    private GameObject getFaceObject(GameObject child){
+        GameObject found = null;
+        foreach(Transform transform in child.transform) {
+            if(transform.CompareTag("Face")){
+                found = transform.gameObject;
+                break;
+            }
+        }
+        return found;
+    }
+
+    private void setFaceRender(){
+         currentMat.color = Color.green;
+         currentMat.SetColor("_EmissionColor", Color.black);
     }
 }
