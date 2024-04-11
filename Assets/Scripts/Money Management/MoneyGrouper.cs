924bcd20e53fd6db9d394ea0f409dc106c1047bb
kennedyat <ktpenny8@gmail.com>
2024-04-10 22:49:35 -0400

    Added Sort

--- Assets/Scripts/Money Management/MoneyGrouper.cs
+++ Assets/Scripts/Money Management/MoneyGrouper.cs
@@ -10,12 +10,19 @@ public class MoneyGrouper : MonoBehaviour
     */
     public List<GameObject> dollars = new List<GameObject>();
 
+    [Header("Dollar Objects")]
+    public GameObject twentyDollar;
+    public GameObject tenDollar;
+    public GameObject fiveDollar;
+    public GameObject oneDollar;
+
     // This dictionary returns a spawn point depenting on the currency value 
     private Dictionary<float, Transform> spawnPoints = new Dictionary<float, Transform>();
 
     public GameObject grouper;
     private GroupValue groupValue;
     private float tempValue;
+    private float spaceBetweenDollars = 2.5f;
 
     // The goal of this method is to stack the money and count its value
     public void onSubmissionViewEvent()
@@ -28,6 +35,45 @@ public class MoneyGrouper : MonoBehaviour
             currency.transform.parent = grouper.transform;
             groupValue.AddValue(tempValue);
             currency.transform.position = spawnPoints[tempValue].position;
+        }
+    }
+
+    public void getDollarGrid(int twenty, int ten, int five, int one){
+        //Add value to list
+        for(int i = 0; i<twenty; i++)
+            dollars.Add(twentyDollar);
+        for(int i = 0; i<ten; i++)
+            dollars.Add(tenDollar);
+        for(int i = 0; i<five; i++)
+            dollars.Add(fiveDollar);
+        for(int i = 0; i<one; i++)
+            dollars.Add(oneDollar);
+
+        setPosition();
+        
+        //Sort values in list
+        // Multiply positions by list index
+    }
+
+    public void sortDollars(){
+        GameObject temp;
+        for(int i = 0; i<dollars.Count; i++){
+            for(int j = 0; j<dollars.Count; j++){
+                if(dollars[j].GetComponent<DollarValue>().value > dollars[i].GetComponent<DollarValue>().value){
+                    temp = dollars[i];
+                    dollars[i] = dollars[j];
+                    dollars[j] = temp;
+                }
+            }
+        }
+    }
+
+    public void setPosition(){
+        float index = 0;
+        foreach(GameObject dollar in dollars){
+            dollar.transform.position  = new Vector3(dollar.transform.position.x + spaceBetweenDollars*index,
+                dollar.transform.position.y, dollar.transform.position.z);
+            index++;
         }
     }
 }
