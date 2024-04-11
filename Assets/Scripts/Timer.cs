42d0c0910b65949263e8d9b14d566535311d9496
kennedyat <ktpenny8@gmail.com>
2024-04-11 17:22:25 -0400

    Added Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public GameObject target;
+    public EventManager _eventManager;
+    float degreePerSecond;
+    // Start is called before the first frame update
+    
+
+    void Start()
+    {
+       degreePerSecond = transform.eulerAngles.z/_eventManager.playTime*1.3f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Time: "+Time.deltaTime);
+         transform.RotateAround(target.transform.position, Vector3.forward, degreePerSecond * Time.deltaTime);
+    }
+}
